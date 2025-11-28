750b87017b9492783274485209535966708e6004
Thyyd <roastingblade@gmail.com>
2025-11-28 17:11:43 +0100

    Ajout fichier print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+* print_rev - prints a string from the end to the beggining
+* @s: Pointer to a string we want to print reverted
+*
+* Return: Noting
+*/
+
+void print_rev(char *s)
+{
+int len = 0;
+while (s[len] != '\0')
+{
+len++;
+}
+for(int i = len; i > 0; i--)
+{
+    write(1, &s[i-1], 1);
+}
+
+write(1, "\n", 1);
+}
