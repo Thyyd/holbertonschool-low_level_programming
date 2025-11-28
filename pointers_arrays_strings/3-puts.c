b9b072b7aa2a113e612914dcd44c9842911fd028
Thyyd <roastingblade@gmail.com>
2025-11-28 13:25:34 +0100

    Ajout fichier puts.c

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* _puts - prints a string
+* @str: Pointer to a string we want to print
+*
+* Return: Noting
+*/
+
+void _puts(char *str)
+{
+int len = 0;
+while (str[len] != '\0')
+{
+    printf("%c", str[len]);
+    len++;
+}
+printf("\n");
+}
