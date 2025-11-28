716bdc5156d697dd464a1f37105d8fd0e2ba1fd6
Thyyd <roastingblade@gmail.com>
2025-11-28 16:56:15 +0100

    Ajout fichier puts.c

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 
 /**
@@ -12,8 +13,18 @@ void _puts(char *str)
 int len = 0;
 while (str[len] != '\0')
 {
-printf("%c", str[len]);
+write(1, &str[len], 1);
 len++;
 }
-printf("\n");
+write(1, "\n", 1);
+}
+
+
+int main(void)
+{
+    char *str;
+
+    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    _puts(str);
+    return (0);
 }
