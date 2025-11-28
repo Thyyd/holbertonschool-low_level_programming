c1fd40df54ae1f90815e40157f462af9c69848fc
Thyyd <roastingblade@gmail.com>
2025-11-28 12:00:05 +0100

    Ajout fichier reset_to_98.c

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
 /**
-* Fonction reset_to_98
-*
-* Cette fonction ne renvoie rien, elle change juste la valeur
-* de la variable int pointée à 98.
-**/
+ * reset_to_98 - Updates the value pointed to by n to 98
+ * @n: Pointer to an integer to update
+ *
+ * Return: Nothing.
+ */
 
 void reset_to_98(int *n)
-/*
-* Cette fonction permet de changer la valeur d'un nombre entier
-* a la valeur 98, et ce quelle que soit sa precedente valeur.
-*
-* Parametres :
-*    - n : pointeur int permettant de modifier la valeur directement
-*          présente a l'adresse de la variable n
-**/
+
 {
 *n = 98;
 }
