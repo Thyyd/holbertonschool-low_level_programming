40459324e814b403d7ee54b0a787659e62a30909
Thyyd <roastingblade@gmail.com>
2025-11-28 11:15:53 +0100

    Ajout fichier reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Fonction reset_to_98
+ *
+ * Cette fonction ne renvoie rien, elle change juste la valeur
+ * de la variable int pointée à 98.
+ **/
+
+void reset_to_98(int *n)
+{
+    *n = 98;
+}
